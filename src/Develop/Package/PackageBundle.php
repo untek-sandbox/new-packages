3cef05fb2759597385c12d3ebb2b6befb65d4d14
Vitaliy <theyamshikov@yandex.ru>
2024-08-18 14:03:43 +0500

    upd

---
+++ src/Develop/Package/PackageBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Untek\Develop\Package;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class PackageBundle extends AbstractBundle
+{
+
+    public function build(ContainerBuilder $container)
+    {
+        $fileLocator = new FileLocator(__DIR__);
+        $loader = new PhpFileLoader($container, $fileLocator);
+        $loader->load(__DIR__ . '/resources/config/services/package.php');
+    }
+}
---
+++ src/Utility/CodeGenerator/CodeGeneratorBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Untek\Utility\CodeGenerator;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class CodeGeneratorBundle extends AbstractBundle
+{
+
+    public function build(ContainerBuilder $container)
+    {
+        $fileLocator = new FileLocator(__DIR__);
+        $loader = new PhpFileLoader($container, $fileLocator);
+        $loader->load(__DIR__ . '/resources/config/services/main.php');
+        $loader->load(__DIR__ . '/../CodeGeneratorApplication/resources/config/services/main.php');
+        $loader->load(__DIR__ . '/../CodeGeneratorCli/resources/config/services/main.php');
+        $loader->load(__DIR__ . '/../CodeGeneratorDatabase/resources/config/services/main.php');
+        $loader->load(__DIR__ . '/../CodeGeneratorRestApi/resources/config/services/main.php');
+    }
+}
