38aed391d21ddf761f9c9b4dc5ae1f19296677e6
Vitaliy <theyamshikov@yandex.ru>
2024-08-24 20:04:43 +0500

    upd

--- src/Utility/CodeGenerator/CodeGenerator/Application/Interfaces/GeneratorInterface.php
+++ src/Utility/CodeGenerator/CodeGenerator/Application/Interfaces/GeneratorInterface.php
@@ -5,9 +5,8 @@ namespace Untek\Utility\CodeGenerator\CodeGenerator\Application\Interfaces;
 use Untek\Utility\CodeGenerator\CodeGenerator\Application\Dto\GenerateResultCollection;
 
 /**
- * @method GenerateResultCollection generate(CommandInterface $command)
+ * @method void generate(CommandInterface $command)
  */
 interface GeneratorInterface
 {
-
 }
